2541cb474e3cff899cec4777923ce4adc2676b2c
Cherag Verma <cherag@ixigo.com>
2019-02-04 18:09:36 +0530

    added heroku config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -50,6 +50,6 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port: 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port: ${port}`)
 })
